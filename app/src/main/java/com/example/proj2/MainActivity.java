cea173501cbb2764e076e88b8a17ad837f8bbc4f
fl_3650 <repilovi@gmail.com>
2024-02-14 21:14:11 +0300

    themes complete. Note: layouts are bad AF

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.proj2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActionBar;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpActionBar();
+    }
+
+    private void setUpActionBar() {
+        ActionBar actionBar = getActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
