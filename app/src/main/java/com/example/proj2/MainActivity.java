4aba4811b74bf0065e7cfcde683563381eacdfc1
fl_3650 <repilovi@gmail.com>
2024-02-15 14:01:25 +0300

    did some stuff

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -1,17 +1,61 @@
 package com.example.proj2;
 
 import android.app.ActionBar;
+import android.content.ContentValues;
+import android.graphics.Camera;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
+    Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        mTextView = findViewById(R.id.hello);
+
         setUpActionBar();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        android.os.Debug.stopMethodTracing();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            // mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mCamera == null) {
+            initializeCamera();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        ContentValues values = new ContentValues();
+        // ...
+    }
+
+    private void initializeCamera() {
     }
 
     private void setUpActionBar() {
