48058c2c58958ab5c4fb297e941aaf026a826138
fl_3650 <repilovi@gmail.com>
2024-02-19 19:45:13 +0300

    did some stuff

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.proj2;
 
 import android.app.ActionBar;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Camera;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.lang.reflect.Field;
@@ -12,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     TextView mTextView;
     Camera mCamera;
 
+    static final String STATE_SCORE = "pS";
+    static final String STATE_LEVEL = "pL";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.hello);
 
-        setUpActionBar();
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+        }
     }
 
     @Override
-    public void onDestroy() {
+    protected void onDestroy() {
         super.onDestroy();
         android.os.Debug.stopMethodTracing();
     }
@@ -39,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
 
         if (mCamera == null) {
@@ -55,6 +66,23 @@ public class MainActivity extends AppCompatActivity {
         // ...
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsEnabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
     private void initializeCamera() {
     }
 
@@ -64,5 +92,12 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    public void onSavedInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(STATE_SCORE, 5);
+        savedInstanceState.putInt(STATE_LEVEL, 5);
+
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
